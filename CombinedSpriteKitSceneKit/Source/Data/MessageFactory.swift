04e09cca2718d97e8b5a1e351021984fd4b9c38f
周博立 <bolizhou@xiaohongshu.com>
2022-02-07 17:54:55 +0800

    chore: msg control

--- CombinedSpriteKitSceneKit/Source/Data/MessageFactory.swift
+++ CombinedSpriteKitSceneKit/Source/Data/MessageFactory.swift
@@ -8,10 +8,99 @@
 
 import Foundation
 
+let kMsgConsumeLength: Int = 10
+let kMsgConsumeInterval: TimeInterval = 1
+
 class MessageFactory: NSObject {
     
+    static let shared: MessageFactory = MessageFactory()
+
+    static var consumer: (([Message]) -> Void)? {
+        get {
+            let shared = MessageFactory.shared
+            return shared.consumer
+        }
+        set {
+            let shared = MessageFactory.shared
+            shared.consumer = newValue
+        }
+    }
+    var userFactory: UserFactory {
+        UserFactory.shared
+    }
+    var consumeLength: Int = kMsgConsumeLength
+    var consumeInterval: TimeInterval = kMsgConsumeInterval
+    var consumer: (([Message]) -> Void)?
+    var consumeTimer: Timer?
+    
+    lazy var dataSource: DataSource = DataSource()
     lazy var caches: [Message] = [Message]()
+    lazy var op: DispatchQueue = DispatchQueue(label: "com.dance.msg.op")
     
+    func receive(_ messages: [Message]) {
+        op.async {
+            self.caches.append(contentsOf: messages)
+            var usrs = [User]()
+            for msg in messages {
+                usrs.append(msg.sender)
+            }
+            self.receive(usrs)
+        }
+    }
     
+    func receive(_ users: [User]) {
+        userFactory.inRoomUsers.append(contentsOf: users)
+        if userFactory.inRoomUsers.count > userFactory.inRoomUsrMaxCount {
+            let diffCount: Int = userFactory.inRoomUsrMaxCount - userFactory.inRoomUsers.count
+            for idx in 0..<diffCount where userFactory.inRoomUsers.count > idx {
+                userFactory.inRoomUsers.removeFirst()
+            }
+        }
+        for usr in userFactory.inRoomUsers {
+            userFactory.inRoomUserMap[usr.uid] = usr
+        }
+    }
+    
+    static func consume(withTimeInterval interval: TimeInterval, length: Int, callback: @escaping (([Message]) -> Void)) {
+        let shared = MessageFactory.shared
+        shared.consumeLength = length
+        shared.startTimer(withConsumer: callback, interval: interval)
+    }
+    
+    func consume(_ callback: @escaping (([Message]) -> Void)) {
+        op.async {
+            if self.caches.count > self.consumeLength {
+                var res: [Message] = [Message]()
+                for (idx, msg) in self.caches.enumerated() where idx < self.consumeLength {
+                    res.append(msg)
+                }
+                for idx in 0..<self.consumeLength where self.caches.count > idx {
+                    self.caches.removeFirst()
+                }
+                callback(res)
+            } else {
+                let res: [Message] = self.caches
+                callback(res)
+                self.caches.removeAll()
+            }
+        }
+    }
+    
+    func stopTimer() {
+        consumeTimer?.invalidate()
+    }
+    
+    func startTimer(withConsumer con: @escaping (([Message]) -> Void), interval: TimeInterval) {
+        consumer = con
+        consumeInterval = interval
+        let timer: Timer = Timer(timeInterval: consumeInterval, repeats: true) { [weak self] _ in
+            self?.consume({ users in
+                self?.consumer?(users)
+            })
+        }
+        RunLoop.main.add(timer, forMode: .common)
+        timer.fire()
+        consumeTimer = timer
+    }
     
 }
--- CombinedSpriteKitSceneKit/Source/Data/UserFactory.swift
+++ CombinedSpriteKitSceneKit/Source/Data/UserFactory.swift
@@ -8,93 +8,11 @@
 
 import Foundation
 
-let kConsumeLength: Int = 5
-let kConsumeInterval: TimeInterval = 1
 let kInRoomUsrMaxCount: Int = 200
 
 class UserFactory: NSObject {
-    
     static let shared: UserFactory = UserFactory()
-    static var consumer: (([User]) -> Void)? {
-        get {
-            let shared = UserFactory.shared
-            return shared.consumer
-        }
-        set {
-            let shared = UserFactory.shared
-            shared.consumer = newValue
-        }
-    }
-    
-    var consumeInterval: TimeInterval = kConsumeInterval
-    var consumeLength: Int = kConsumeLength
-    var inRoomUsrMaxCount: Int = kInRoomUsrMaxCount
-    var consumer: (([User]) -> Void)?
-    var consumeTimer: Timer?
-    
-    lazy var dataSource: DataSource = DataSource()
     lazy var inRoomUsers: [User] = [User]()
     lazy var inRoomUserMap: [String: Any] = [String: Any]()
-    lazy var op: DispatchQueue = DispatchQueue(label: "com.dance.data.op")
-    
-    static func consume(withTimeInterval interval: TimeInterval, length: Int, callback: @escaping (([User]) -> Void)) {
-        let shared = UserFactory.shared
-        shared.consumeLength = length
-        shared.startTimer(withConsumer: callback, interval: interval)
-    }
-    
-    func consume(_ callback: @escaping (([User]) -> Void)) {
-        op.async {
-            if self.inRoomUsers.count > self.consumeLength {
-                var res: [User] = [User]()
-                for (idx, usr) in self.inRoomUsers.enumerated() where idx < self.consumeLength {
-                    res.append(usr)
-                }
-                for idx in 0..<self.consumeLength where self.inRoomUsers.count > idx {
-                    if let usr = self.inRoomUsers.first {
-                        self.inRoomUserMap.removeValue(forKey: usr.uid)
-                        self.inRoomUsers.removeFirst()
-                    }
-                }
-                callback(res)
-            } else {
-                let res: [User] = self.inRoomUsers
-                self.inRoomUsers.removeAll()
-                self.inRoomUserMap.removeAll()
-                callback(res)
-            }
-        }
-    }
-    
-    func produce(_ users: [User]) {
-        op.async {
-            self.inRoomUsers.append(contentsOf: users)
-            if self.inRoomUsers.count > self.inRoomUsrMaxCount {
-                let diffCount: Int = self.inRoomUsrMaxCount - self.inRoomUsers.count
-                for idx in 0..<diffCount where self.inRoomUsers.count > idx {
-                    self.inRoomUsers.removeFirst()
-                }
-            }
-            for usr in self.inRoomUsers {
-                self.inRoomUserMap[usr.uid] = usr
-            }
-        }
-    }
-    
-    func stopTimer() {
-        consumeTimer?.invalidate()
-    }
-    
-    func startTimer(withConsumer con: @escaping (([User]) -> Void), interval: TimeInterval) {
-        consumer = con
-        consumeInterval = interval
-        let timer: Timer = Timer(timeInterval: consumeInterval, repeats: true) { [weak self] _ in
-            self?.consume({ users in
-                self?.consumer?(users)
-            })
-        }
-        RunLoop.main.add(timer, forMode: .common)
-        timer.fire()
-        consumeTimer = timer
-    }
+    var inRoomUsrMaxCount: Int = kInRoomUsrMaxCount
 }
